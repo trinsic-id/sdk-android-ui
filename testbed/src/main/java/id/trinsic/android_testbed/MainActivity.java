55d9f36cf16d8b54e004f45242cd79b296bbc066
Joshua Coffey <josh@trinsic.id>
2024-08-29 13:08:28 -0400

    Testbed improvements

--- testbed/src/main/java/id/trinsic/android_testbed/MainActivity.java
+++ testbed/src/main/java/id/trinsic/android_testbed/MainActivity.java
@@ -6,16 +6,29 @@ import id.trinsic.android.ui.TrinsicClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import id.trinsic.android_testbed.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
-    private static String LAUNCH_URL = "https://josh.trinsic-local.com/api/mobiletest?sessionId=1234";
+    // Replace the below with a URL that, when called with a GET request, will return a session launch URL as the only text content of the response.
+    // It will likely do so by using the Trinsic backend API SDK to create a session and return the launch URL.
+    private static String BACKEND_CREATE_SESSION_ENDPOINT = "";
 
+    // Replace the below with a URL that uses a custom scheme that you've properly registered in your app's AndroidManifest.xml
+    // The path (in this case "/callback") can be anything.
+    private static String CALLBACK_REDIRECT_URL = "trinsic-android-ui-testbed-redirect-scheme:///callback";
+
+    private ActivityMainBinding binding;
     private TrinsicClient trinsicClient;
 
     @Override
@@ -42,8 +55,35 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d("OnClick", "Invoking Trinsic client");
-                trinsicClient.Invoke(LAUNCH_URL, "trinsic-testbed");
+                String launchUrl;
+                try {
+                    launchUrl = createLaunchUrl();
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Failed to create launch URL: " + e.getMessage(), Toast.LENGTH_LONG);
+                    throw new RuntimeException(e);
+                }
+
+                trinsicClient.Invoke(launchUrl, CALLBACK_REDIRECT_URL);
             }
         });
+    }
+
+    private String createLaunchUrl() throws Exception {
+        // Horrible hack to allow us to do networking on the UI thread since this is a simple sample
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        StringBuilder result = new StringBuilder();
+        HttpURLConnection conn = (HttpURLConnection) new URL(BACKEND_CREATE_SESSION_ENDPOINT).openConnection();
+        conn.setRequestMethod("GET");
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+        }
+
+        return result.toString();
     }
 }
