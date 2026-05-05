01cee1f87909fe1f49fbda82594ec8a41c53d63f
Joshua Coffey <josh+github@trinsic.id>
2026-05-05 16:36:12 -0400

    Update testbed dependency

--- testbed/build.gradle.kts
+++ testbed/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("androidx.navigation:navigation-ui:2.7.7")
-//    implementation("com.github.trinsic-id:sdk-android-ui:0.0.4")
+//    implementation("id.trinsic:sdk-android-ui:2.1.0-alpha2")
     implementation(project(":library"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
